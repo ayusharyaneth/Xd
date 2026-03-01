4192c776940a6042055416fa5659abb887499084
Ayush <ayusharyaneth@gmail.com>
2026-03-01 18:26:03 +0530

    Create wallet_cluster.py

---
+++ engines/wallet_cluster.py
@@ -0,0 +1,5 @@
+class WalletClusterEngine:
+    @staticmethod
+    def detect(pair_data: dict) -> float:
+        # Mock cluster detection score (0 = no clusters, 100 = highly clustered/sybil)
+        return 15.0
